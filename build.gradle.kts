40928e5a246117312ae6b2b55f8dd0a5f6eac316
Alexander Koval <kovalidis@gmail.com>
2025-07-17 18:10:06 +0300

    Set number of test cluster nodes to 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ configureOpensearchPlugin(
     name = "collapse-extension",
     description = "Adds rescorer for mixing up search hits inside their groups.",
     classname = "dev.evo.opensearch.collapse.CollapseRescorePlugin",
+    numberOfTestClusterNodes = 2,
 )
 
 version = Versions.project
